88a50c4bae0c3796fbefafc62ba3e0c07c7f2cd0
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2024-08-08 00:52:47 -0600

    update ticks
    Co-Authored-By: Diego Morales <77738746+Diego2250@users.noreply.github.com>
    Co-Authored-By: Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>

--- MiniProyecto/main.c
+++ MiniProyecto/main.c
@@ -98,7 +98,7 @@ int main() {
                 }
             }
         }
-        if (cycle % 500 == 0) {
+        if (cycle % 5000 == 0) {
             printf("Ciclo %d:\n", cycle);
             print_ecosystem(&ecosystem);
         }
